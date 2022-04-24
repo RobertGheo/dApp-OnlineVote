5e2d8ec59a06ca73e2c7353af95631fd76c453af
RobertGheo <robert.bradu@yahoo.com>
2022-04-24 03:03:27 +0100

    updated gui

--- src/pages/AlreadyVote/index.tsx
+++ src/pages/AlreadyVote/index.tsx
@@ -4,10 +4,20 @@ import {
   useGetAccountInfo,
   useGetNetworkConfig
 } from '@elrondnetwork/dapp-core';
+import { Modal } from 'react-bootstrap';
 
 const VoteDone = () => {
   const { address } = useGetAccountInfo();
   const { network } = useGetNetworkConfig();
+  const [success, setSuccess] = React.useState(false);
+
+  function closePopMessage() {
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
+    setSuccess(false);
+  }
+  function popUpMessage() {
+    setSuccess(true);
+  }
 
   return (
     <div className='container pt-5 mt-5'>
@@ -23,7 +33,7 @@ const VoteDone = () => {
                   <h1 className='text-center p-3 p-lg-4 '>
                     Thank you for voting.
                   </h1>
-                  <div className='mx-4 my-2 pt-5 opacity-6'>
+                  <div className='mx-4 my-2 pt-4 opacity-6'>
                     <span className='h4'>Your address and ID:&nbsp;</span>
                     <span data-testid='accountAddress'>
                       <a
@@ -49,7 +59,7 @@ const VoteDone = () => {
                     </span>
                   </div>
                   <div className=''>
-                    <h3 className='text-center mx-3 m-lg-4 py-3 px-lg-4'>
+                    <h3 className='text-center mx-3 m-lg-2 py-3 px-lg-4'>
                       Thankfulness for maintaining that very spirit of
                       democracy, by actually casting your very vote; which
                       actually holds so very much importance.
@@ -62,7 +72,7 @@ const VoteDone = () => {
           <div className='card-body d-felx flex-column m-2 p-4'>
             <div className='card bg-light p-lg-5 border-0'>
               <div className='card bg-secondary p-2 p-lg-5 shadow'>
-                <div className='card bg-warning p-2 m-2 col-9 col-md-6 col-lg-3'>
+                <div className='card bg-warning p-2 m-2 col-sm-10 col-md-6 col-lg-4'>
                   <h2 className='m-1 my-2 font-weight-bold text-center'>
                     Complaints
                   </h2>
@@ -72,11 +82,33 @@ const VoteDone = () => {
                     If you wish to make a complaint about the elections or
                     referendum please use&nbsp;
                     <a
-                      href=''
-                      className='text-monospace font-weight-bold badge badge-warning '
+                      onClick={popUpMessage}
+                      className='btn text-monospace font-weight-bold badge badge-warning '
                     >
                       this form.
                     </a>
+                    {success && (
+                      <Modal show={true} className='p-5'>
+                        <Modal.Header className='badge badge-secondary'>
+                          <div className='h3 p-2 mx-2 mt-2 mb-0 text-center'>
+                            Further development
+                          </div>
+                        </Modal.Header>
+                        <Modal.Body className='h2 p-5 m-2 text-center'>
+                          TSI Online Voting System will be updated and improved
+                          in the following months.&nbsp; Thank you for your
+                          time.
+                        </Modal.Body>
+                        <Modal.Footer>
+                          <button
+                            className='btn-success p-2 px-3 mx-3 my-2 rounded h3'
+                            onClick={closePopMessage}
+                          >
+                            Close
+                          </button>
+                        </Modal.Footer>
+                      </Modal>
+                    )}
                   </h3>
                 </div>
               </div>
